cf86866d4c19630d4db763d7cda6d649d5606177
mehravarun666 <mehra.varun666@gmail.com>
2024-11-09 16:55:16 +0530

    Initial Commit

---
+++ CryCurCon/CryCurConApp.swift
@@ -0,0 +1,18 @@
+//
+//  CryCurConApp.swift
+//  CryCurCon
+//
+//  Created by Varun Mehra on 09/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CryCurConApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PortfolioView()
+        }
+    }
+}
+
---
+++ CryCurConTests/CryCurConTests.swift
@@ -0,0 +1,17 @@
+//
+//  CryCurConTests.swift
+//  CryCurConTests
+//
+//  Created by Varun Mehra on 09/11/24.
+//
+
+import Testing
+@testable import CryCurCon
+
+struct CryCurConTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
