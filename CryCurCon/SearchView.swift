3d16842604a0c423760c9f0612d798b24894b7d5
mehravarun666 <mehra.varun666@gmail.com>
2024-11-11 20:31:40 +0530

    bottom navigation baar added

---
+++ CryCurCon/SearchView.swift
@@ -0,0 +1,17 @@
+// SearchView.swift
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("Search for something here")
+            .font(.largeTitle)
+            .padding()
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
